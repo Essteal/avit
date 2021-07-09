4f56b8e2e9fc838a828e6ca099fdbe9038faf1ec
Essteal <87208068+Essteal@users.noreply.github.com>
2021-07-09 23:22:43 +0300

    Add files via upload

---
+++ 123/configav.py
@@ -0,0 +1,8 @@
+import aiohttp
+
+DB_URL = 'postgresql://postgres:123@localhost:5432/parser'
+BOT_TOKEN = '1843626565:AAFit33-TvglI-bGRKYHGYmvvebOaow3T3o'
+AVITO_TOKEN = 'okbcodes2aj919ajsjab'
+ADMIN_USERNAME = '@hui'
+ADMINS = (934540473)  #
+session = aiohttp.ClientSession()
